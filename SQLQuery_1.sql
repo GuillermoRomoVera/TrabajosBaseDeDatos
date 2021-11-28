966c057b392da1804a849f7be78c97e404a43cff
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2021-11-27 18:21:41 -0600

    cambios

---
+++ SQLQuery_1.sql
@@ -0,0 +1,5 @@
+insert into viaje values(100428525, 'Avenida Tecnologico #107', '0:30 min',$30, 29438483, '211H002', '1U49US484' );
+insert into viaje values(203852940,'Avenida Vicente Guerrero #1819', '0:15 min',$32 , 372654596, '200H922', '4A24FA854');
+insert into viaje values(028582658, 'Calle Abraham Lincoln #1917','1:15 min',$62, 482847357, '200H111', '4U74MX2642' );
+insert into viaje values(183029584,'Calle Madero #3520', '0:12 min',$20, 82757272, '200S519', '0M84MX772');
+insert into viaje values(82746365, 'Calle Ignacio Zaragoza #4103', '0:45 min' , $55, 82746365, '210H329', '4A49MX811' );
